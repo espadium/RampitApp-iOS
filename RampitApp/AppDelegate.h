1a45aa3b088444e3ee87df1ec45aef4bfe605cb1
Gabriel Osorio <gabriel.osorio@neonroots.com>
2014-08-18 21:08:28 -0300

    Initial Commit

---
+++ RampitApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RampitApp
+//
+//  Created by Gabriel Osorio on 8/18/14.
+//  Copyright (c) 2014 AccesibleUY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RampitApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RampitApp
+//
+//  Created by Gabriel Osorio on 8/18/14.
+//  Copyright (c) 2014 AccesibleUY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
